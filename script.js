543d16d964fa1594cddd2040b976fb029d67e8af
yasminemanuely14 <yasmin.lima.santos14@escola.pr.gov.br>
2026-06-01 15:23:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+javascript
+function escolhaCorreta() {
+    let resposta = document.getElementById("resultado-desafio");
+    resposta.innerHTML = "Excelente! 🔋 Descarte o e-lixo corretamente. Componentes químicos pesados contaminam o solo se forem para aterros normais.";
+    resposta.style.color = "#00796B";
+}
 
+function escolhaIncorreta() {
+    let resposta = document.getElementById("resultado-desafio");
+    resposta.innerHTML = "Ação inadequada! 🌍 Lixo eletrônico comum polui a natureza ou fica acumulado sem utilidade. Tente o descarte correto.";
+    resposta.style.color = "#D32F2F";
+}
